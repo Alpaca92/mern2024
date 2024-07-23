5803c338e4f59de917a6cf8235469164d910d483
Alpaca92 <zxcvbnm5288@gmail.com>
2024-07-24 08:50:10 +0900

    feat: login jwt token

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import { config } from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
 
 config();
 const app = express();
 
 // NOTE: middleware
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // remove it in production
 app.use(morgan('dev'));
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { compare, hash } from 'bcrypt';
 import User from '../models/User.js';
+import { createToken } from '../utils/token-manager.js';
+import { COOKIE_NAME } from '../utils/constants.js';
 
 export const getAllUsers = async (
   req: Request,
@@ -34,6 +36,23 @@ export const userSignup = async (
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
 
+    // NOTE create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      domain: 'localhost',
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, '7d');
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: 'OK', id: user._id.toString() });
   } catch (error) {
     console.log(error);
@@ -63,6 +82,22 @@ export const userLogin = async (
         .status(401)
         .json({ message: 'ERROR', cause: 'Invalid credentials' });
     }
+
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      domain: 'localhost',
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, '7d');
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      signed: true,
+    });
 
     return res.status(200).json({ message: 'OK', id: user._id.toString() });
   } catch (error) {
